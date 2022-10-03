efdc3c9a7627123bc7846e7a0853020c62a65fec
Roman Yacenko <ryacenko3@gmail.com>
2022-10-03 17:13:42 +0300

    first part

--- src/components/TableOfNumbers/index.js
+++ src/components/TableOfNumbers/index.js
@@ -8,29 +8,15 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 
 export const TableOfNumbers = ({ text }) => {
   const [ objOfSymbols, setObjOfSymbols ] = useState({});
+  const [ amountOfInfo, setAmountOfInfo ] = useState(0);
 
   useEffect(() => {
-    // (
-    //   async () => {
-    //     const res = await fetch("https://libretranslate.com/translate", {
-    //       method: "POST",
-    //       body: JSON.stringify({
-    //         q: text,
-    //         source: "en",
-    //         target: "ua",
-    //         format: "text",
-    //       }),
-    //       headers: { "Content-Type": "application/json" }
-    //     });
-
-    //     console.log(res);
-    //   }
-    // )()
-
     let obj = {};
+    let info = 0;
 
     text.split('').forEach((item) => {
       if (item in obj) {
@@ -50,10 +36,26 @@ export const TableOfNumbers = ({ text }) => {
         return 1;
       }
     });
-    
+
+    for (let i = 0; i < Object.keys(obj).length; i++) {
+      info += obj[Object.keys(obj)[i]] * Math.log2(obj[Object.keys(obj)[i]]);
+    }
+
+    setAmountOfInfo(-1 * info);
     setObjOfSymbols({ ...obj });
   }, [ text ])
 
+  const copyHandler = () => {
+    if (!Object.keys(objOfSymbols)) {
+      return;
+    }
+
+    const str = Object.keys(objOfSymbols)
+      .map(item => `${item}: ${objOfSymbols[item]}`)
+      .join('\n');
+
+    navigator.clipboard.writeText(str)
+  }
 
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -76,29 +78,55 @@ export const TableOfNumbers = ({ text }) => {
   }));
 
   return(
-    <TableContainer component={Paper} aria-label="customized table">
-      <Table sx={{ minWidth: 320 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell align="center">symbol</StyledTableCell>
-            <StyledTableCell align="center">Count</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {Object.keys(objOfSymbols).length
-            ? Object.keys(objOfSymbols).map((item, index) => (
-              <StyledTableRow
-                key={index}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <StyledTableCell align="center">{item}</StyledTableCell>
-                <StyledTableCell align="center">{objOfSymbols[item]}</StyledTableCell>
-              </StyledTableRow>
-            ))
-            : ''
-          }
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <TableContainer component={Paper} aria-label="customized table" style={{ marginBottom: '20px'}}>
+        <Table sx={{ minWidth: 320 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="center">Shannon formule</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <StyledTableCell align="center">{amountOfInfo}</StyledTableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TableContainer component={Paper} aria-label="customized table">
+        <Table sx={{ minWidth: 320 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="center">symbol</StyledTableCell>
+              <StyledTableCell align="center">Count</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Object.keys(objOfSymbols).length
+              ? Object.keys(objOfSymbols).map((item, index) => (
+                <StyledTableRow
+                  key={index}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <StyledTableCell align="center" style={{ padding: "16px 8px"}}>{item}</StyledTableCell>
+                  <StyledTableCell align="center">{objOfSymbols[item]}</StyledTableCell>
+                </StyledTableRow>
+              ))
+              : ''
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Button
+        size="medium"
+        color="secondary"
+        onClick={copyHandler}
+        loadingPosition="start"
+        variant="contained"
+        style={{margin: '10px 0 20px' }}
+      >
+        Copy
+      </Button>
+    </>
   )
 }
